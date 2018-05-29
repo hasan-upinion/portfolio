aedab7142a8718b8509f360bbe9b61f583937a67
Hasan_sh <hasansh343@gmail.com>
2018-05-29 16:44:49 +0200

    Created my amazing portfolio..

---
+++ data.js
@@ -0,0 +1,85 @@
+'use strict'
+
+// I don't need a database, because my content is mere static.
+
+const skills = [
+  {
+    title: 'HTML',
+    percentage: 75
+  },
+
+  {
+    title: 'css',
+    percentage: 85
+  },
+  {
+    title: 'js',
+    percentage: 80
+  },
+  {
+    title: 'nodejs',
+    percentage: 75
+  },
+  {
+    title: 'react',
+    percentage: 90
+  },
+  {
+    title: 'angular',
+    percentage: 65
+  },
+  {
+    title: 'mysql',
+    percentage: 60
+  },
+  {
+    title: 'cms',
+    percentage: 50
+  }
+]
+
+const projects = [
+  {
+    name: 'Dathuis',
+    src: './assets/dathuis.PNG',
+    description:
+      'DatHuis was established to provide insight into the quality and services of brokers.',
+    position:
+      'I have done an intrenship at dathuis for 6 months as a front/back-end dev. We used ReactJs and Redux in the front-end and for the back-end NodeJS and MongoDB.',
+    url: 'www.dathuis.nl'
+  },
+  {
+    name: 'Hyfer',
+    src: './assets/hyfer.PNG',
+    description:
+      'Hyfer is a timeline for the modules that are given at hackyourfuture. Hackyourfuture is an orgenization that teaches regugees coding, and find them jobs.',
+    position:
+      'I built it along with two other colleuges of mine. I did both the back and front end. AngularJs and material design used on the front-end, NodeJS was the back-end and mySql was in the database. https://github.com/hyf-project5/hyfer',
+    url: 'http://hyfer.hackyourfuture.net/timeline'
+  },
+  {
+    name: 'Simple Todo App',
+    src: './assets/todo.png',
+    description:
+      'Orgenizing schedules and have a good overview over my appointments I built this app.',
+    position:
+      'This app has two versions. One with is Angular and Css, and the other is with Angular and Angular Material.',
+    url: 'https://github.com/hasanshahoud/todoApp.AngularJs2'
+  },
+  {
+    name: 'See my Other repositories.',
+    src: './assets/repos.PNG',
+    description: 'Visit my repositories and other projects I am involved in.',
+    position: 'owner',
+    url: 'https://github.com/hasanshahoud'
+  },
+  {
+    name: 'Crossarx',
+    src: '',
+    description:
+      'The CrossmarX Application Platform enables you and your customers to grow your business.',
+    position:
+      'I have done an intrenship with Crossmarx for about 3 months, and worked on several projects using velocity which is a Java-based template engine. Along with html&css.',
+    url: 'https://www.crossmarx.nl/web/home.vm'
+  }
+]
---
+++ index.js
@@ -0,0 +1,143 @@
+'use strict'
+
+const menuButton = document.querySelector('.menuButton')
+const menu = document.querySelector('.menu')
+const skillsEl = document.querySelector('.skills')
+const projectsEl = document.querySelector('.projects')
+const contactForm = document.querySelector('#contact-form')
+const modal = document.querySelector('#modal')
+
+menuButton.addEventListener('click', toggleMenu)
+document.body.addEventListener('click', hideMenu)
+contactForm.addEventListener('submit', onContactSubmit)
+document.addEventListener('scroll', e => {
+  const body = document.body,
+    html = document.documentElement
+
+  const fullHeight = Math.max(
+    body.scrollHeight,
+    body.offsetHeight,
+    html.clientHeight,
+    html.scrollHeight,
+    html.offsetHeight
+  )
+
+  const scrolledDown = html.scrollTop
+  const clientHeight = document.documentElement.clientHeight
+
+  if (scrolledDown + clientHeight > fullHeight - 70) {
+    menuButton.classList.add('hide')
+  } else if (menuButton.classList.contains('hide')) {
+    menuButton.classList.remove('hide')
+  }
+})
+
+function toggleMenu(e) {
+  e.stopPropagation()
+  menuButton.classList.toggle('clicked')
+  if (menu.classList.contains('show')) {
+    menu.classList.add('hide')
+    menu.classList.remove('show')
+  } else {
+    menu.classList.remove('hide')
+    menu.classList.add('show')
+  }
+}
+
+function hideMenu() {
+  if (menu.classList.contains('show')) {
+    menuButton.classList.remove('clicked')
+    menu.classList.add('hide')
+    menu.classList.remove('show')
+  }
+}
+
+function onContactSubmit(e) {
+  let name = e.target[0].value
+  let email = e.target[1].value
+  let message = e.target[2].value
+  e.target.action = `mailto:hasansoso343@gmail.com?subject=Hi Hasan!&body=Hi this is ${name},%0D%0A%0D%0A${message}`
+  const success = document.querySelector('#success')
+  success.classList.add('show')
+}
+
+function openModal(id) {
+  const project = projects.find(project => project.src === id)
+  // console.log(project)
+  modal.style.display = 'flex'
+  modal.classList.add('show')
+  const modalContent = modal.querySelector('.modalContent')
+  const header = modal.querySelector('.modalHeader')
+  const body = modal.querySelector('.modalBody')
+  const footer = modal.querySelector('.modalFooter')
+
+  const img = `
+    <img src="${id ? id : './assets/noImage.png'}" class="modalHeaderImage" >
+  `
+  const headerText = `<h3 class="modalHeaderText">${project.name}</h3>`
+  const bodyText = `
+    <p class="position">${project.position}</p>
+  `
+  const link = `<a  href="${
+    project.url
+  }" target="_blank" class="link">See website</a>`
+
+  header.innerHTML = `
+    ${img} 
+    <p>${project.description}</p> ${headerText}
+  `
+  body.innerHTML = `${bodyText} `
+  footer.innerHTML = `<button class="closeModal">Close</button> ${link}`
+  footer.querySelector('.closeModal').addEventListener('click', closeModal)
+
+  modal.addEventListener('click', handleModalClick)
+}
+
+function handleModalClick(e) {
+  if (e.target.id === 'modal') {
+    closeModal()
+  }
+}
+
+function closeModal() {
+  const hide = setTimeout(() => {
+    modal.style.display = 'none'
+    clearTimeout(hide)
+  }, 400)
+  modal.classList.remove('show')
+  modal.removeEventListener('click', handleModalClick)
+}
+
+let skillsHtml = ''
+for (const skill of skills) {
+  const html = `
+    <div class="skillTitle">${skill.title}</div>
+    <div class="percentage">
+    <span class="skillPercentageIndicator" style="width: ${
+      skill.percentage
+    }%;"></span>
+    <span class="skillPercentage">${skill.percentage}%</span>
+                    </div>
+                    `
+  skillsHtml += html
+}
+
+skillsEl.innerHTML = skillsHtml
+
+let projectsHtml = ''
+for (const project of projects) {
+  const html = `
+  <div class="project animated fade-in" id="${project.src}">
+  <img class="projectImg ${!project.src ? 'noImage' : ''}" src="${
+    project.src ? project.src : './assets/noImage.png'
+  }" alt="${project.name} Photo" >
+  <h3 class="projectTitle">${project.name}</h3>
+  <button class="learnMore"  id="${
+    project.src
+  }" onclick="openModal(this.id)">Read More!</button>
+  </div>
+  `
+  projectsHtml += html
+}
+
+projectsEl.innerHTML = projectsHtml
