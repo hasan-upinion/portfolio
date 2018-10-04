0313a0bbbbb3a23aee2bbeac9c3afcd3e6571b33
Hasan_sh <hasansh343@gmail.com>
2018-10-04 14:39:25 +0200

    Fix link in the modal description

--- data.js
+++ data.js
@@ -3,99 +3,100 @@
 // I don't need a database, because my content is mere static.
 
 const skills = [
-  {
-    title: 'js',
-    percentage: 90
-  },
-  {
-    title: 'python',
-    percentage: 65
-  },
-  {
-    title: 'nodejs',
-    percentage: 75
-  },
-  {
-    title: 'react',
-    percentage: 95
-  },
-  {
-    title: 'react native',
-    percentage: 65
-  },
-  {
-    title: 'angular',
-    percentage: 65
-  },
-  {
-    title: 'mysql',
-    percentage: 60
-  },
-  {
-    title: 'webpack',
-    percentage: 70
-  },
-  {
-    title: 'redux',
-    percentage: 80
-  },
-  {
-    title: 'HTML',
-    percentage: 75
-  },
+    {
+        title: 'js',
+        percentage: 90,
+    },
+    {
+        title: 'python',
+        percentage: 65,
+    },
+    {
+        title: 'nodejs',
+        percentage: 75,
+    },
+    {
+        title: 'react',
+        percentage: 95,
+    },
+    {
+        title: 'react native',
+        percentage: 65,
+    },
+    {
+        title: 'angular',
+        percentage: 65,
+    },
+    {
+        title: 'mysql',
+        percentage: 60,
+    },
+    {
+        title: 'webpack',
+        percentage: 70,
+    },
+    {
+        title: 'redux',
+        percentage: 80,
+    },
+    {
+        title: 'HTML',
+        percentage: 75,
+    },
 
-  {
-    title: 'css',
-    percentage: 85
-  },
-  {
-    title: 'cms',
-    percentage: 50
-  }
+    {
+        title: 'css',
+        percentage: 85,
+    },
+    {
+        title: 'cms',
+        percentage: 50,
+    },
 ]
 
 const projects = [
-  {
-    name: 'Dathuis',
-    src: './assets/dathuis.PNG',
-    description:
-      'DatHuis was established to provide insight into the quality and services of brokers.',
-    position:
-      'I have done an intrenship at dathuis for 6 months as a front/back-end dev. We used ReactJs and Redux in the front-end and for the back-end NodeJS and MongoDB.',
-    url: 'https://www.dathuis.nl'
-  },
-  {
-    name: 'Hyfer',
-    src: './assets/hyfer.PNG',
-    description:
-      'Hyfer is a timeline for the modules that are given at hackyourfuture. Hackyourfuture is an orgenization that teaches regugees coding, and find them jobs.',
-    position:
-      'I built it along with two other colleuges of mine. I did both the back and front end. AngularJs and material design used on the front-end, NodeJS was the back-end and mySql was in the database. https://github.com/hyf-project5/hyfer',
-    url: 'http://hyfer.hackyourfuture.net/timeline'
-  },
-  {
-    name: 'Chat App',
-    src: './assets/chatApp.png',
-    description:
-      'This is my first experience with React Native. I wanted to make a simple app to try RN out, but now I am using it with my friends(instead of WhatsApp :) ).',
-    position:
-      'I have built it with the great server provider Firebase. It has many features; like authentication, local storage, and a smooth navigation flow. Check: https://expo.io/@hasan-sh/chatApp',
-    url: 'https://github.com/hasanshahoud/Chatting_RN'
-  },
-  {
-    name: 'See my other repositories.',
-    src: './assets/repos.PNG',
-    description: 'Visit my repositories and other projects I am involved in.',
-    position: 'owner',
-    url: 'https://github.com/hasanshahoud'
-  },
-  {
-    name: 'Crossarx',
-    src: '',
-    description:
-      'The CrossmarX Application Platform enables you and your customers to grow your business.',
-    position:
-      'I have done an intrenship with Crossmarx for about 3 months, and worked on several projects using velocity which is a Java-based template engine. Along with html&css.',
-    url: 'https://www.crossmarx.nl/web/home.vm'
-  }
+    {
+        name: 'Dathuis',
+        src: './assets/dathuis.PNG',
+        description:
+            'DatHuis was established to provide insight into the quality and services of brokers.',
+        position:
+            'I have done an intrenship at dathuis for 6 months as a front/back-end dev. We used ReactJs and Redux in the front-end and for the back-end NodeJS and MongoDB.',
+        url: 'https://www.dathuis.nl',
+    },
+    {
+        name: 'Hyfer',
+        src: './assets/hyfer.PNG',
+        description:
+            'Hyfer is a timeline for the modules that are given at hackyourfuture. Hackyourfuture is an orgenization that teaches regugees coding, and find them jobs.',
+        position:
+            'I built it along with two other colleuges of mine. I did both the back and front end. AngularJs and material design used on the front-end, NodeJS was the back-end and mySql was in the database. <a target="_blank" href="https://github.com/hyf-project5/hyfer">HYFER</a>',
+        url: 'http://hyfer.hackyourfuture.net/timeline',
+    },
+    {
+        name: 'Chat App',
+        src: './assets/chatApp.png',
+        description:
+            'This is my first experience with React Native. I wanted to make a simple app to try RN out, but now I am using it with my friends(instead of WhatsApp :) ).',
+        position:
+            'I have built it with the great server provider Firebase. It has many features; like authentication, local storage, and a smooth navigation flow. Check: <a target="_blank" href="https://expo.io/@hasan-sh/chatApp">Hier</a>',
+        url: 'https://github.com/hasanshahoud/Chatting_RN',
+    },
+    {
+        name: 'See my other repositories.',
+        src: './assets/repos.PNG',
+        description:
+            'Visit my repositories and other projects I am involved in.',
+        position: 'owner',
+        url: 'https://github.com/hasanshahoud',
+    },
+    {
+        name: 'Crossarx',
+        src: '',
+        description:
+            'The CrossmarX Application Platform enables you and your customers to grow your business.',
+        position:
+            'I have done an intrenship with Crossmarx for about 3 months, and worked on several projects using velocity which is a Java-based template engine. Along with html&css.',
+        url: 'https://www.crossmarx.nl/web/home.vm',
+    },
 ]
